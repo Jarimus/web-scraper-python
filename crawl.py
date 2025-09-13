ec7506e43399eb53378fb068ca7f73faac2d4197
Jari Ahonen <jari.n.ahonen@gmail.com>
2025-09-13 14:56:27 +0300

    crawl_page function that recursively crawls pages of a website

--- crawl.py
+++ crawl.py
@@ -63,21 +63,19 @@ def get_images_from_html(html: str, base_url: str) -> list[str]:
 
     return result
 
-def extract_page_data(html: str, page_url: str):
-    url = get_urls_from_html(html, page_url)
+def extract_page_data(html: str, page_url: str) -> dict[str, str | list[str]]:
     h1 = get_h1_from_html(html)
     first_paragraph = get_first_paragraph_from_html(html)
     links = get_urls_from_html(html, page_url)
     img_urls = get_images_from_html(html, page_url)
 
-    data = {
+    return {
         "url": page_url,
         "h1": h1,
         "first_paragraph": first_paragraph,
         "outgoing_links": links,
         "image_urls": img_urls
     }
-    return data
     
 def get_html(url: str) -> tuple[str, Exception | None]:
     res = requests.get(
@@ -92,9 +90,45 @@ def get_html(url: str) -> tuple[str, Exception | None]:
     # Make sure the response is text/html
     content_type = res.headers.get("Content-Type")
     if content_type and content_type.find("text/html") == -1:
-        return "", ConnectionError("page is not text/html")
+        return "", ConnectionError(f"page is not text/html (is {content_type})")
     # Attempt to read the content
     try:
         return res.content.decode(), None
     except Exception as err:
         return "", err
+    
+def crawl_page(base_url: str, current_url=None, page_data=None, checked_urls: set[str]=set()):
+    if current_url is None:
+        current_url = base_url
+    if page_data is None:
+        page_data = {}
+    
+    # Make sure current_url is in the same domain
+    if not current_url.startswith(base_url):
+        return
+
+    # Get normalized version of current_url. Add it to checked urls
+    normalized_url = normalize_url(current_url)
+    checked_urls.add(normalized_url)
+
+    # If the normalized url is already in page_data, return
+    if page_data.get(normalize_url, None):
+        return
+    # Get html from the url. Print statement to keep track of the crawl.
+    print(f"Getting html for {current_url}")
+    html, err = get_html(current_url)
+    if err:
+        print(f"Invalid page: {err}")
+        return
+    # extract_page_data() to get rich data from the page, add to page_data[normalized_url] = rich_data
+    rich_data = extract_page_data(html, current_url)
+    page_data[normalized_url] = rich_data
+    # Get all urls from response body html
+    urls = rich_data.get("outgoing_links", [])
+    # Recursively call each of them (current_url is the new page)
+    for url in urls:
+        if page_data.get(normalize_url(url), None) or normalize_url(url) in checked_urls:
+            continue
+        checked_urls.add(url)
+        crawl_page(base_url, url, page_data, checked_urls)
+    return page_data
--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import sys
-from crawl import get_html
+from crawl import crawl_page
 
 def main():
+    # Ensure correct arg: website
     if len(sys.argv) < 2:
         print("no website provided")
         sys.exit(1)
     if len(sys.argv) > 2:
         print("too many arguments provided")
         sys.exit(1)
-    
     base_url = sys.argv[1]
     print(f"starting crawl of: {base_url}")
-    content, err = get_html(base_url)
-    if err:
-        print(f"Error fetching website: {err}")
-        sys.exit(1)
-    print(content)
+    
+    # Crawl the website
+    data = crawl_page(base_url)
+    if data:
+        n = len(data)
+        print("*"*20)
+        print(f"Number of pages: {n}")
+        print("Titles:")
+        for rich_data in data.values():
+            print(rich_data["h1"])
+    else:
+        print("Nothing retrieved.")
 
 
 if __name__ == "__main__":
